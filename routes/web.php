f1033cff7dcbe676c3b79a0e0ca87bec85170cb0
Saidy-lucas <lucassaidy@gmail.com>
2025-10-07 16:04:42 +0100

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,6 @@
 <?php
+
+use App\Http\Controllers\Backend\AdminController;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
@@ -11,6 +13,12 @@ foreach(File::allFiles(__DIR__ .'/web') as $route_file){
 }
 
 require __DIR__.'/auth.php';
+
+//Routa admin login
+Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
+
+//Routa admin recuperação de  senha
+Route::get('admin/forgot-password', [AdminController::class, 'forgot'])->name('admin.forgot');
 
 
 
